f1602cb47ca1c1396791efde7181214ff603a0f5
Landgraf Paul <landgraf.paul@gmail.com>
2018-06-21 13:22:18 +0200

    Initial commit

---
+++ plugin.js
@@ -0,0 +1,48 @@
+const assert = require('assert'),
+	express = require('express'),
+	cors = require('cors'),
+	expressDomainMiddleware = require('express-domain-middleware'),
+	http = require('http'),
+	bodyParser = require('body-parser'),
+	path = require('path'),
+	methodOverride = require('method-override'),
+	passport = require('passport')
+
+module.exports = function () {
+	assert(!this.express, "field exists")
+	this.express = express();
+	this.passport = passport;
+	this.express.use(cors());
+	this.express.use(expressDomainMiddleware);
+	this.express.use(bodyParser.urlencoded({ extended: true, parameterLimit: 5000 }));
+	this.express.use(bodyParser.json({ limit: '1mb' }));
+	this.express.use(methodOverride());
+	this.express.engine('html', require('ejs').renderFile);
+
+	this.express.set('view engine', 'ejs');
+	this.express.set('views', this.config.get("project.www"));
+	this.express.use(express.static(this.config.get("project.www")));
+
+
+	assert(!this.server, "field exists")
+	this.server = http.createServer(this.express);
+
+	this.on("ready", () => {
+		this.express.use((error, req, res, next) => {
+			if (error.name === 'UnauthorizedError') {
+				res.status(error.status).send({ message: error.message });
+				return;
+			}
+			next();
+		});
+
+		this.server.listen(this.config.get("project.port"), (err) => {
+			if (err) {
+				return console.error("server", err)
+			}
+			console.info("listening", this.config.get("project.port"));
+		});
+	});
+
+	return Promise.resolve();
+}
