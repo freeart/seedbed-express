86bf2eb87448dd9da276b3b6a27c71438a8a355f
Pavel Nekrasov <pavel@resultdevelopment.com>
2018-08-20 12:05:05 +0200

    added ssl

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const assert = require('assert'),
 	express = require('express'),
 	cors = require('cors'),
 	expressDomainMiddleware = require('express-domain-middleware'),
+	fs = require('fs'),
 	http = require('http'),
+	https = require('https'),
 	bodyParser = require('body-parser'),
 	methodOverride = require('method-override'),
 	passport = require('passport')
@@ -11,6 +13,7 @@ module.exports = function () {
 	assert(!this.express, "field exists")
 	assert(!this.passport, "field exists")
 	assert(!this.server, "field exists")
+	assert(!this.serverSSL, "field exists")
 
 	this.express = express();
 	this.passport = passport;
@@ -23,7 +26,20 @@ module.exports = function () {
 
 	this.express.set('view engine', 'ejs');
 	this.express.set('views', this.config.get("project.www"));
-	this.express.use(this.config.get("project.basepath"), express.static(this.config.get("project.www")));	
+	this.express.use(this.config.get("project.basepath"), express.static(this.config.get("project.www")));
+
+	if (this.config.get("project.ssl")) {
+		const privateKey = fs.readFileSync(this.config.get("project.ssl.privkey"), 'utf8');
+		const certificate = fs.readFileSync(this.config.get("project.ssl.cert"), 'utf8');
+		const ca = fs.readFileSync(this.config.get("project.ssl.chain"), 'utf8');
+
+		const credentials = {
+			key: privateKey,
+			cert: certificate,
+			ca: ca
+		};
+		this.serverSSL = https.createServer(credentials, this.express);
+	}
 
 	this.server = http.createServer(this.express);
 
@@ -42,6 +58,15 @@ module.exports = function () {
 			}
 			console.info("listening", this.config.get("project.express"));
 		});
+
+		if (this.serverSSL) {
+			this.serverSSL.listen(this.config.get("project.expressSSL"), (err) => {
+				if (err) {
+					return console.error("server ssl", err)
+				}
+				console.info("listening", this.config.get("project.expressSSL"));
+			});
+		}
 	});
 
 	return Promise.resolve();
